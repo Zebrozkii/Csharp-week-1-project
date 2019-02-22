6cb5ba2b80b7ac7dbed162984bd8addb0a4eabed
Jimmy Zebroski <zebrozkii@gmail.com>
2019-02-22 09:42:29 -0800

    Got multiple test done makes sure user input is correct, checks for zer matched words, then a test to verify the count is working

--- WordCounter.Test/ModelTest/WordCounterTest.cs
+++ WordCounter.Test/ModelTest/WordCounterTest.cs
@@ -7,13 +7,33 @@ namespace WordCounter.Tests
   public class WordCounterTest
   {
     [TestMethod]
-    public void IsUserInputString()
-    {
-      string testWord="hello";
-      string testSentence="My name is jimmy";
-      CountWords testWordCount = new CountWords();
-       Assert.AreEqual(true, testWordCount.WordCounter(testWord));
 
+    public void GetUserSentence()
+    {
+      string sentence="hello im jimmy";
+      string word="jimmy";
+      CountWords test=new CountWords(word,sentence);
+      int count=test.WordCount();
+      Assert.AreEqual(count,1);
+    }
+    [TestMethod]
+  
+    public void ZeroMatchedWords()
+    {
+      string sentence="hello im jimmy";
+      string word="water";
+      CountWords test=new CountWords(word,sentence);
+      int count=test.WordCount();
+      Assert.AreEqual(count,0);
+    }
+    [TestMethod]
+    public void MakeSureCountWorks()
+    {
+      string sentence="hello im im im jimmy";
+      string word="im";
+      CountWords test=new CountWords(word,sentence);
+      int count=test.WordCount();
+      Assert.AreEqual(count,3);
     }
   }
 }
--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -2,14 +2,33 @@ namespace WordCounter
 {
   public class CountWords
   {
-    private int _counter;
     private string _userInputWord;
     private string _userInputSentence;
-    public CountWords(int counter, string word, string sentence){
-      _counter = counter;
+
+    public CountWords( string word, string sentence){
       _userInputWord = word;
       _userInputSentence = sentence;
     }
-    
+    public string UserSentence(){
+
+      return _userInputSentence;
+
+    }
+    public string UserWord(){
+      return _userInputWord;
+    }
+    public int WordCount(){
+      int wordCount=0;
+      string[] sentenceSplit=_userInputSentence.Split(' ');
+      foreach(string x in sentenceSplit)
+      {
+        if(x==_userInputWord)
+        {
+          wordCount++;
+        }
+      }
+      return wordCount;
+    }
+
   }
 }
