4ae95881891ce4ad13b4a4c10494884c6ec25a2c
Jimmy Zebroski <zebrozkii@gmail.com>
2019-02-25 20:01:42 -0800

    Added more cshtml pages to views

---
+++ WordCounter/Controllers/WordCounterController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using WordCounter.Models;
+using System.Collections.Generic;
+
+namespace WordCounter.Controllers
+{
+  public class WordCounterController : Controller
+  {
+    [HttpGet("/items")]
+    public ActionResult Index()
+    {
+        List<CountWords> allItems = CountWords.GetAll();
+        return View(allItems);
+    }
+
+    [HttpPost("/items")]
+    public ActionResult Create(string sentence, string word)
+    {
+        CountWords myItem = new CountWords(word,sentence);
+        int number = myItem.CountWords(word,sentence);
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet("/items/new")]
+    public ActionResult New()
+    {
+        return View();
+    }
+
+    [HttpPost("/items/delete")]
+    public ActionResult DeleteAll()
+    {
+        CountWords.ClearAll();
+        return View();
+    }
+
+    [HttpGet("/items/{id}")]
+    public ActionResult Show(int id)
+    {
+        CountWords item = CountWords.Find(id);
+        return View(item);
+    }
+
+  }
+}
--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -1,15 +1,19 @@
 using System.Collections.Generic;
+using WordCounter;
 
 namespace  WordCounter.Models
 {
   public class CountWords
   {
     private string _userInputWord;
+    private int _id;
     private string _userInputSentence;
+    private static List<CountWords> _instances = new List<CountWords>{};
 
     public CountWords( string word, string sentence){
       _userInputWord = word;
       _userInputSentence = sentence;
+      _id = _instances.Count;
     }
     public string UserSentence(){
 
@@ -18,6 +22,19 @@ namespace  WordCounter.Models
     }
     public string UserWord(){
       return _userInputWord;
+    }
+
+    public static List<CountWords>GetAll()
+    {
+      return _instances;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
     }
     public int WordCount(){
       int wordCount=0;
